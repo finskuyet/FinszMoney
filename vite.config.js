2144db240df15098f418588ae5299e1a0d452377
Finskuyet <findranugraha13@gmail.com>
2026-08-30 11:23:29 +0700

    fix: add multiple HTML entry points for Vite build

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   server: {
     port: 3000
   },
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        login: resolve(__dirname, 'login.html'),
+        register: resolve(__dirname, 'register.html')
+      }
+    }
   }
 })
